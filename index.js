858c5270ce4ea8023932cb79094aa2f0c40ca2f2
Tom Taylor <hello@tomtaylor.name>
2020-08-19 23:06:26 +0100

    Add test github API

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 const express = require('express');
+const github = require('./lib/github');
 
 const config = require('./config.json');
 
-function init() {
-	console.log('Hello Pi!');
-	const app = express();
-
-	app.use(express.static('public'))
-
-	app.get('/', (req, res, next) => {
+const routes = {
+	'/': (req, res, next) => {
 		console.log('Served Homepage');
 		res.render('index.html');
-	});
-	app.get('/api', (req, res, next) => {
-		console.log('Served API');
-		res.json({
-			hello: 'pi!',
-			route: req.route
-		});
-	});
+	},
+	'/github': async (req, res, next) => {
+		github.getTotalContributions().then(c => res.send(c > 3000)).catch(next);
+	}
+};
 
-	app.listen(config.port, console.log(`listening on ${config.port}`));
+function init() {
+	const app = express();
+	app.use(express.static('public'))
+	Object.entries(routes).forEach(([route, handler]) => app.get(route, handler));
+	app.listen(config.port, console.log(`Hello Pi!\nlistening on ${config.port}`));
 }
 
 init();
